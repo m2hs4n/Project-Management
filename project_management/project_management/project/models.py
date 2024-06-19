ecb6139245bbfbc5075623f6819f76c134b7f283
m2hs4n <m2hs4n@gmail.com>
2024-06-19 20:50:51 +0330

    feat:(Project,Task,Comment)Create models

--- project_management/project_management/project/models.py
+++ project_management/project_management/project/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    class Status(models.TextChoices):
+        PENDING = 'PE', 'Pending'
+        APPROVED = 'IN', 'In Progress'
+        REJECTED = 'CO', 'Completed'
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+    title = models.CharField(max_length=255)
+    status = models.CharField(max_length=3, choices=Status.choices, default=Status.PENDING)
+    due_date = models.DateField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title}-{self.status}-{self.due_date}'
+
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=255)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    
